115b431ec7b3112655ef4e6d62eedeed5dae5b59
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2023-07-18 15:32:18 +0530

    update settings

--- bhavans/settings/base.py
+++ bhavans/settings/base.py
@@ -26,11 +26,14 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "registration.apps.RegistrationConfig",
     "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
--- bhavans/settings/prod.py
+++ bhavans/settings/prod.py
@@ -1,4 +1,5 @@
 from .base import *
+from decouple import config
 
 DATABASES = {
     "default": {
@@ -16,3 +17,20 @@ DEBUG = False
 #         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
 #     ]
 # }
+
+CORS_ALLOW_ORIGIN_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
+
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# EMAIL_BACKEND = "mailer.backend.DbBackend"IL_PORT')
+EMAIL_USE_TLS = True
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# EMAIL_BACKEND = "mailer.backend.DbBackend"
+
+EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = config("EMAIL_PORT")
+EMAIL_USE_TLS = True
