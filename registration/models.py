8e98e393d93416da5c4627d12b1601b97bd579f8
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2023-08-02 16:59:00 +0530

    add image fields in REG

--- registration/models.py
+++ registration/models.py
@@ -56,6 +56,7 @@ class Registration(models.Model):
 
     first_name = models.CharField(max_length=50, null=True, blank=True)
     last_name = models.CharField(max_length=50, null=True, blank=True)
+    image = models.ImageField(upload_to="media", null=True, blank=True)
     email = models.EmailField(max_length=254, null=True, blank=True)
     dob = models.DateField(null=True, blank=True)
 
