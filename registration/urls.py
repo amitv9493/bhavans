33ab687911b0ebb120bf68088115584031a00a26
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2023-08-02 15:30:51 +0530

    add trailing slash

--- registration/urls.py
+++ registration/urls.py
@@ -19,5 +19,5 @@ urlpatterns = [
     path("payment/<int:registration_id>/", Payment, name="payment"),
     path("payment/complete/", PaymentView.as_view()),
     path("refer/", ReferenceCreateView),
-    path("refer/<int:pk>", ReferenceGETView),
+    path("refer/<int:pk>/", ReferenceGETView),
 ] + router.urls
