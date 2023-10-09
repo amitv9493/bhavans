4a0bf3a121f4656cab2903e22ad131d82a391ea4
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2023-10-09 10:29:10 +0530

    Update payment serializer event field

--- registration/serializers.py
+++ registration/serializers.py
@@ -4,6 +4,7 @@ from drf_writable_nested.serializers import WritableNestedModelSerializer
 
 
 class PaymentSerializer(serializers.ModelSerializer):
+    event = serializers.StringRelatedField()
     class Meta:
         model = Payment
         fields = "__all__"
@@ -86,19 +87,7 @@ class RegistrationGETSerializer(serializers.ModelSerializer):
     class Meta:
         model = Registration
         fields = "__all__"
-        # read_only_fields = ["events"]
-
-        # extra_kwargs = {"guest": {"read_only": True}}
-
-    # def update(self, instance, validated_data):
-    #     guest_data = validated_data.pop("guest",[])
-    #     # event_data =
-
-    #     if len(guest_data) >0:
-    #         for guest in guest_data:
-    #             Guest.objects.get_or_create(**guest)
-
-    #     return super().update(instance, validated_data)
+    
 
 
 # class RegistrationGETSerializer(serializers.ModelSerializer):
